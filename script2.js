d108f2e20def4a8b549deeb6c95eb847faaa027f
sidonbids <122887513+sidonbids@users.noreply.github.com>
2023-01-23 12:53:17 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+var embedConfig = {"hash":"ek02886","width":1920,"height":1080,"t":"{CACHE_BUSTER}","userId":843078,"network":"STANDARD","type":"html5"};
+</script>
+<script type="text/javascript" src="https://live-tag.creatopy.net/embed/embed.js"></script>
